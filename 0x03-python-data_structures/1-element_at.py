29cf3bffdd6efb2880b3d3f1ea06a60ecb576eb9
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-07-03 10:42:29 +0000

    Retrieves an element from a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,9 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list) - 1:
         return None
-    i = 0
-    while i < len(my_list):
-        if i == idx:
-            return my_list[i]
+    for element in my_list:
+        if element == my_list[idx]:
+            return element
             break
-        i += 1
