6d6cfef3acafc9f9b8a8b8da7be8c16940ddbad3
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-07-03 11:28:03 +0000

    Replace an element of a list in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    new_list = []
+    for num in my_list:
+        if num == my_list[idx]:
+            new_list.append(element)
+        else:
+            new_list.append(num)
+    return new_list
