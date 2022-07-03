0a3149afcf4145f0c37e276d8f0557ac491dbdfe
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-07-03 10:37:39 +0000

    Retrieves an element in a particular index

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0 or idx > len(my_list) - 1:
+        return None
+    i = 0
+    while i < len(my_list):
+        if i == idx:
+            return my_list[i]
+            break
+        i += 1
