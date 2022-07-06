c8b3824eab62917223fa3b7c69345405bed19bbc
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-07-06 21:27:49 +0000

    Deleted

--- 0x04-python-more_data_structures/7-updated_dictionary.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def update_dictionary(a_dictionary,  key, value):
-    if key not in a_dictionary.keys():
-        a_dictionary.update({key: value})
-    else:
-        a_dictionary[key] = value
-    for keys, values in a_dictionary.items():
-        print("{}: {}".format(keys, values))
