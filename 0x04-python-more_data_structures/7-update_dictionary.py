753affc5afa961d397b3763f04ad5d6c95a445f7
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-07-06 21:33:25 +0000

    Replaces or adds key/value in a dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary,  key, value):
     a_dictionary[key] = value
-    for keys, values in a_dictionary.items():
-        print("{}: {}".format(keys, values))
+    return a_dictionary
