3163ab057b1e7644db1c9b5759d8439e9c61ef66
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-07-06 22:13:37 +0000

    Returns key with max value

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    max_score = max(a_dictionary.values())
+    for key, value in a_dictionary.items():
+        if max_score == value:
+            return key
