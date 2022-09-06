b44697748e02d240a9084522e681f3cfba9d632b
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-09-06 21:15:11 +0000

    Lists all records in a preferred order

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,7 @@
+-- lists all records of table second_table
+-- results should display both score and name(in this order)
+-- records should be ordered by score(top first)
+
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
