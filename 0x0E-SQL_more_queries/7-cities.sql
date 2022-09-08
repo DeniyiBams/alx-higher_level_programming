05a057bcaf85463d518617a5e7e787e309bcab87
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-09-08 22:36:14 +0000

    error fix

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,8 +3,8 @@
 -- state_id INT, can't be null and must be a foreign key that references to id of the states table
 -- name VARCHAR(256) can't be null
 
-CREATE DATABASE IF NOT EXISTS hbtn-0d_usa;
-CREATE TABLE IF NOT EXISTS htbtn_0d_usa.cities(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 state_id INT NOT NULL,
 name VARCHAR(256) NOT NULL,
-FOREIGN KEY(state_id) REFERENCES states(id));
+FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
