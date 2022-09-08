caa5a8c22c4292db24aeb1d233d5bb99be40446f
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-09-08 22:08:38 +0000

    Creates a table in MySQL server with a unique default decription

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,4 @@
+-- creates the table unique_id
+-- Description: id INT with default value and must be unique, name VARCHAR(256)
+
+CREATE TABLE IF NOT EXISTS unique_id(id INT UNIQUE DEFAULT 1, name VARCHAR(256);
