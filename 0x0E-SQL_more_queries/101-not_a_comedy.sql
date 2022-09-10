2bf28dcfe6ed3eca7244929e365721dd69ebdea7
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-09-10 12:35:57 +0000

    Error Fix

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,8 +1,8 @@
 -- lists all shows without the genre Comedy
 
-SELECT tv_shows.title AS title FROM tv_shows WHERE tv_shows.id
-NOT IN (SELECT tv_shows.id from tv_shows inner join tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id INNER JOIN tv_genres ON
-tv_genres.id = tv_show_genres.genre_id
+SELECT tv_shows.title FROM tv_shows WHERE tv_shows.id
+NOT IN (SELECT tv_shows.id from tv_shows INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_genres.name = "Comedy")
 ORDER BY tv_shows.title;
