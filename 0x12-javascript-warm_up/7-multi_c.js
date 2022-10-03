1670781b0998814bb8328be2de95e25eebfeb3b8
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-10-03 21:00:32 +0000

    Error fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,19 +1,12 @@
 #!/usr/bin/node
 // prints x times 'C is fun'
 
-const myVar = 'C is fun';
 const num = process.argv[2];
 
 if (typeof num === 'undefined') {
   console.log('Missing number of occurences');
-}
-
-if (num > 0) {
-  let i = 0;
-  while (i < num) {
-    console.log(myVar);
-    i++;
-  }
 } else {
-  return;
-}
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
+} 
