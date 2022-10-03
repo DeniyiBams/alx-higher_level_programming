5e7e3e5b8183ec0a00eb9834c67841eaf189e061
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-10-03 21:47:10 +0000

    Error fix

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,14 +1,8 @@
 #!/usr/bin/node
 // prints addition of 2 integers
 
-function add(a, b) {
-  return a + b;
+function add (a, b) {
+  console.log(a + b);
 }
-const num1 = parseInt(process.argv[2]);
-const num2 = parseInt(process.argv[3]);
 
-if (process.argv.length < 4) {
-  console.log('NaN');
-} else {
-  console.log(add(num1, num2));
-}
+add(parseInt(process.argv[2], 10), parseInt(process.argv[3]));
