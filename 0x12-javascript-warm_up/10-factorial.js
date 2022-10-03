fcb17d95afd7cca17fce1f12412de6a05e09e36c
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-10-03 22:04:25 +0000

    Error fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -5,12 +5,10 @@ function factorial(num) {
   if (num === 1 || num === 0) {
     return 1;
   }
-
   return num * factorial(num - 1);
 }
 
 const factNum = parseInt(process.argv[2]);
-
 if (isNaN(factNum)) {
   console.log(1);
 } else {
