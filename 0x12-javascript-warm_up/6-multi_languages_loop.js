96e6c285476037f3ce952dc848eb0e03908f5b21
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-10-03 20:45:14 +0000

    Prints message using loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Prints message using loop
+
+const myMessage = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < myMessage.length; i++) {
+  console.log(myMessage[i]);
+}
