59944beabf421183a784388a187f27515e099245
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-10-03 21:59:17 +0000

    Prints the factorial of a number

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// computes and print factorial
+
+function factorial(num) {
+  if (num === 1 || num === 0) {
+    return 1;
+  }
+
+  return num * factorial(num - 1);
+}
+
+const factNum = parseInt(process.argv[2]);
+
+if (isNaN(factNum)) {
+  console.log(1);
+} else {
+  console.log(factorial(factNum));
+}
