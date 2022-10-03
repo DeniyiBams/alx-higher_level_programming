b6aad13c1c70cba9bc8bf67447f98ec997631769
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-10-03 20:11:45 +0000

    Error fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 // Prints message
 
 const argvLength = process.argv.length;
-if(argvLength < 3) {
+if (argvLength < 3) {
   console.log('No argument');
-} else if(argvLength === 3) {
+} else if (argvLength === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
