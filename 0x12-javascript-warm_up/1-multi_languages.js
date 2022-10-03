2c30cc790598c7e6401e65d42ef98a22371f2490
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-10-03 12:54:48 +0000

    Print

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
 'use strict';
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+console.log('C is fun\nPython is cool\nJavascript is amazing');
