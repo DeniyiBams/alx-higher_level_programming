614af307541b7057ef99647d7c889414dfa84007
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-10-05 21:46:01 +0000

    Error fix

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (!(w <= 0 || h <= 0 || (typeof w === 'undefined' || typeof h === 'undefined'))) {
       this.width = w;
       this.height = h;
