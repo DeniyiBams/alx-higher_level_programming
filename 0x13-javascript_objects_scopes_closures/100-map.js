df66362c64a72784dfab83ab9b338339584869bc
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-10-06 11:31:10 +0000

    variable name changed

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 let list = require('./100-data.js').list;
 console.log(list);
-let newList = list.map((num, index) => num * index);
+let newList = list.map((n, index) => n * index);
 console.log(newList);
