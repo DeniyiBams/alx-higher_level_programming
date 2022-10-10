854b19b75a98bdf951a2405de1ad1f2b5b69b97c
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-10-10 00:38:42 +0000

    Prints all states that start with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+python script to list all states that starts with N from a database
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host=localhost, port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
+    cur = db.cursor
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N' ORDER BY id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
