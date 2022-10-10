40e1102b6ce6ddcb456c4dbcc4ea092b8846071c
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-10-10 11:06:53 +0000

    Error fix

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-lists all states with a name starting with N (upper N)
-from the database
+lists all cities from database
 """
 
 import MySQLdb
@@ -10,7 +9,7 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
     JOIN states ON cities.state_id = states.id ORDER BY cities.id")
     rows = cur.fetchall()
     for row in rows:
