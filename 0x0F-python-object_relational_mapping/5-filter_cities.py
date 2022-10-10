290052cf13593f5bf28ae0dd0192742fb024db98
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-10-10 11:27:28 +0000

    Error rectified

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,13 +7,13 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
     cur.execute("SELECT cities.name FROM cities \
     JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
     ORDER BY cities.id", (argv[4],))
     rows = cur.fetchall()
-    for city in rows:
-        print(", ".join(city[0]))
+    print(", ".join(city[0] for city in rows))
     cur.close()
     db.close()
