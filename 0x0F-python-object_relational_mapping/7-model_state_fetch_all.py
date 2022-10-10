9c50aa6f1b992b15b9af64615b8d70cadf66cf60
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-10-10 19:37:04 +0000

    Lists all state using sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Lists all State objects from the database
+"""
+
+import sqlalchemy
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).order_by(state.id):
+        print("{}: {}".format(state.id, state.name))
+    session.close()
