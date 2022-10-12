272edc32ea330baec03a244d1e9b06e7b79e3271
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-10-12 13:15:18 +0000

    Deletes state with a match SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+lists all State objects from a database
+"""
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
+
+if __name__ == "__main__":
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    match = '%a%'
+    states = session.query(State).filter_by(State.name.like(match))
+    for state in states:
+        session.delete(state)
+    session.commit()
+    session.close()
