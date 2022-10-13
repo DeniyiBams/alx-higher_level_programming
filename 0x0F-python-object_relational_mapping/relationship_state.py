42a7e949bab93c8a327ffee93234ed832f4ad16c
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-10-13 10:19:08 +0000

    Class definition of state

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Contains the class definition of a Sate and an instance
+Base = declarative_base()
+"""
+
+import sqlalchemy
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State (Base):
+    """ Definition of class State"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state")
