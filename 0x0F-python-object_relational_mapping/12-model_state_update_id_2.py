ada38e1acd014711c0406f99b28625627b911184
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-10-13 09:41:09 +0000

    Error fix

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(eng)
     Session = sessionmaker(bind=eng)
     session = Session()
-    state = session.query(State).filter_by(State.id=2).first()
+    state = session.query(State).filter_by(id=2).first()
     state.name = 'New Mexico'
     session.commit()
     session.close()
