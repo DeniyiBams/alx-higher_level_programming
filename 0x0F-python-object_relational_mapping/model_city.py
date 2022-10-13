17fabae1d0f2886d79ac4057a549011ae51d9829
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-10-13 09:59:23 +0000

    Error fix

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
 Contains the class definition of a City and an instance
-Base = declarative_base()
 """
 
 import sqlalchemy
@@ -11,8 +10,7 @@ from model_state import Base, State
 
 class City (Base):
     """ Definition of class City"""
-    __tablename__ = 'cities'
-
+    __tablename__ = "cities"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'))
